7f9be70c08b8f564e69593f6b7819e704601ad41
NIKHILLL <nikhilr8447@gmail.com>
2026-09-18 19:30:28 +0530

    Create largest_of_3_numbers.java

---
+++ Java learnings/conditions/largest_of_3_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class largest_of_3_numbers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println(max);
+    }
+}
