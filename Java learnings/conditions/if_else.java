1314b3a11bd8a836d1a8c631433679f39b5f2f2a
NIKHILLL <nikhilr8447@gmail.com>
2026-09-18 18:48:02 +0530

    Create if_else.java

---
+++ Java learnings/conditions/if_else.java
@@ -0,0 +1,11 @@
+public class if_else {
+    public static void main(String[] args) {
+int salary = 25000;
+
+if (salary > 20000) {
+    salary = salary + 3000;
+}
+
+System.out.println(salary);
+    }
+}
